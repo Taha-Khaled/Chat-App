684bce3a790a8f2de3cbb29c4b051b64a3c0807e
taha apps <appsbytaha@gmail.com>
2022-02-15 05:31:58 +0200

    Updates

---
+++ precache-manifest.fd018582fa9dbd16c01e54c7090167c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ed00362ae06dffc29e8eec950cb75f5",
+    "url": "/Chat-App/index.html"
+  },
+  {
+    "revision": "14c3491c0b1d457aa88d",
+    "url": "/Chat-App/static/css/main.37448acd.chunk.css"
+  },
+  {
+    "revision": "36588fb804010fcc4a2a",
+    "url": "/Chat-App/static/js/2.c6ceedb8.chunk.js"
+  },
+  {
+    "revision": "563cec0a8f134cb3c9b5211a1992f1ed",
+    "url": "/Chat-App/static/js/2.c6ceedb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14c3491c0b1d457aa88d",
+    "url": "/Chat-App/static/js/main.a9e61a99.chunk.js"
+  },
+  {
+    "revision": "e54c119b47a47cde4815",
+    "url": "/Chat-App/static/js/runtime-main.562d2f4c.js"
+  }
+]);
